cd749614f7621738dc490b5aa014bb8433afd717
Sugata <sarkarsugata57@gmail.com>
2023-05-29 11:41:39 +0530

    making the navbar responsive

---
+++ script.js
@@ -0,0 +1,22 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        if (nav.classList.contains('active')) {
+            nav.classList.remove('active');
+        } 
+        else {
+            nav.classList.add('active');
+        }
+    });
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        if (nav.classList.contains('active')) {
+            nav.classList.remove('active');
+        }
+    });
+}
